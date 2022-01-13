ee2854f5a0eb314cf09c036c016520812175f4ce
lilyum0 <229867@edu.p.lodz.pl>
2022-01-13 20:28:01 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ou_clion_git.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ou_clion_git.dir/main.c.obj"
+  "libou_clion_git.dll.a"
+  "ou_clion_git.exe"
+  "ou_clion_git.exe.manifest"
+  "ou_clion_git.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ou_clion_git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ou_clion_git.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ou_clion_git.
+# This may be replaced when dependencies are built.
