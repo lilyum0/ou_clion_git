7c999f9b96559c5782d5c68b8dfa5aba55fcfa55
lilyum0 <229867@edu.p.lodz.pl>
2022-01-21 19:56:17 +0100

    metoda policz - zmiana na mnozenie

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+int count(int a, int b){
+    return a * b;
+}
+
 int main() {
     printf("Hello, World!\n");
+    printf("w pierwszym branchu 2*3 = %d", count(2,3));
     return 0;
 }
